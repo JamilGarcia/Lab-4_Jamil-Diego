dfe4d9032e52dc40976a768448f62e8d5f019226
Diego Varela <diegocrash124@gmail.com>
2020-02-14 13:50:29 -0600

    Se crearon los jugadores

---
+++ src/lab/pkg4_jamil/diego/Guardian.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg4_jamil.diego;
+
+/**
+ *
+ * @author diego
+ */
+public class Guardian extends Jugador {
+
+    public Guardian(String nombre, int año, String casa, int num_uniforme) {
+        super(nombre, año, casa, num_uniforme);
+    }
+    
+}
---
+++ src/lab/pkg4_jamil/diego/Jugador.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg4_jamil.diego;
+
+/**
+ *
+ * @author diego
+ */
+public class Jugador {
+    private String nombre;
+    private int año;
+    private String casa;
+    private int num_uniforme;
+
+    public Jugador(String nombre, int año, String casa, int num_uniforme) {
+        this.nombre = nombre;
+        this.año = año;
+        this.casa = casa;
+        this.num_uniforme = num_uniforme;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
+
+    public int getNum_uniforme() {
+        return num_uniforme;
+    }
+
+    public void setNum_uniforme(int num_uniforme) {
+        this.num_uniforme = num_uniforme;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", a\u00f1o=" + año + ", casa=" + casa + ", num_uniforme=" + num_uniforme + '}';
+    }
+    
+}
